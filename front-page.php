a8e7b937553b0216ba61bf9f28c469c4b79711bb
Samanta-Petrova <samanta0950@gmail.com>
2026-05-24 23:15:13 +0200

    hero sektion og intro

--- front-page.php
+++ front-page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <?php get_template_part('template-parts/hero'); ?>
+<?php get_template_part('template-parts/intro'); ?>
 <?php get_template_part('template-parts/services'); ?>
 <?php get_template_part('template-parts/cases'); ?>
 
---
+++ template-parts/hero.php
@@ -0,0 +1,15 @@
+<section class="hero rainbow-bg">
+    <div class="hero-container">
+
+        <div class="hero-content">
+            <h1>Design med fokus på dig </h1>
+            <p>Kreativ og professionel hjemmeside og visuel identitet, til dig der vil blive mere synlig online</p>
+
+            <a href="/kontakt" class="hero-btn">Book et møde</a>
+            <a href="/løsninger" class="hero-btn">Se løsninger</a>
+        </div>
+
+    
+
+    </div>
+</section>
