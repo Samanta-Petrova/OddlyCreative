0bbdce65d2b5151fafa58b15b71e1293b577fe53
Samanta-Petrova <samanta0950@gmail.com>
2026-05-24 20:04:11 +0200

    logo tilføjes

--- template-parts/navigation.php
+++ template-parts/navigation.php
@@ -1,16 +1,16 @@
 <nav class="main-nav">
     <div class="nav-container">
         <a href="<?php echo home_url(); ?>" class="logo">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.svg" alt="Logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/OCLogo.png" alt="Logo">
         </a>
-
+    
         <div class="menu-wrapper">
-            <h1> hey </h1>
+            <h1>hey</h1>
             <?php
             wp_nav_menu(array(
                 'theme_location' => 'primary',
-                'menu_class' => 'main-menu',
-                'container' => false
+                'menu_class'     => 'main-menu',
+                'container'      => false,
             ));
             ?>
         </div>
