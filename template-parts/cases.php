cf488d8338b176a70b432850863747b59518b2d6
Samanta-Petrova <samanta0950@gmail.com>
2026-05-25 21:55:41 +0200

    arbejde med cards
    kan ikke få lo**et til at virke, så nu er jeg lidt ked af det

--- template-parts/cases.php
+++ template-parts/cases.php
@@ -1,18 +1,34 @@
 <section class="cases-section">
-    <div class="container">
+    <div class="container box">
         <h2>Cases</h2>
     </div>
 
-    <div class="cases-grid">
-        <div class="card">
-            <h3>KJ Lash & brow lami</h3>
+    <div class="cases-grid box">
+        <div class="imgBx">
+            <img class="pil-ikon" src="<?php echo get_template_directory_uri(); ?>/assets/images/pilNed.svg" alt="Pil ikon">
         </div>
-        <div class="card">
-            <h3>KJ Lash & brow lami</h3>
+        <div class="card details">
+            <h3>KJ Lash & brow lami
+                <span>logo</span>
+            </h3>
         </div>
-        <div class="card">
-            <h3>KJ Lash & brow lami</h3>
+        <div class="cases-grid box">
+        <div class="imgBx">
+            <img class="pil-ikon" src="<?php echo get_template_directory_uri(); ?>/assets/images/pilNed.svg" alt="Pil ikon">
         </div>
+        <div class="card details">
+            <h3>KJ Lash & brow lami
+                <span>logo</span>
+            </h3>
+        </div><div class="cases-grid box">
+        <div class="imgBx">
+            <img class="pil-ikon" src="<?php echo get_template_directory_uri(); ?>/assets/images/pilNed.svg" alt="Pil ikon">
+        </div>
+        <div class="card details">
+            <h3>KJ Lash & brow lami
+                <span>logo</span>
+            </h3>
+        </div>
+        
     </div>
 </section>
-
