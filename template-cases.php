4fecd990cf1c5aa303567a2c8a0e2f3dc4cefd0e
Samanta-Petrova <samanta0950@gmail.com>
2026-05-28 23:34:50 +0200

    arbejde med tilføjelse af dynamisk cases og archive sider

---
+++ template-cases.php
@@ -0,0 +1,30 @@
+<?php
+/* 
+Template Name: Cases Template 
+*/
+get_header();
+?>
+
+<section class="cases-list">
+    <?php
+    $cases = new WP_Query(array(
+        'post_type' => 'post',
+        'posts_per_page' => -1,
+        'category_name' => 'cases'
+    ));
+
+    if ($cases->have_posts()):
+        while ($cases->have_posts()): $cases->the_post(); ?>
+        
+            <article class="case-item">
+                <h2><?php the_title(); ?></h2>
+                <a href="<?php the_permalink(); ?>">Læs mere</a>
+            </article>
+
+        <?php endwhile;
+    endif;
+    wp_reset_postdata();
+    ?>
+</section>
+
+<?php get_footer(); ?>
